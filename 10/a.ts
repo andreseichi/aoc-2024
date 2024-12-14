86e718a93412c7c43746a5635519735325888857
tashiro <andre_seichi@hotmail.com>
2024-12-14 01:10:50 -0300

    day 10

---
+++ 10/a.ts
@@ -0,0 +1,64 @@
+// const lines: string[] = (await Bun.file("./example.txt").text()).split("\n");
+const lines: string[] = (await Bun.file("./input.txt").text()).split("\n");
+
+console.time("Time to compute");
+
+const checkOnLimit = (x: number, y: number) =>
+  x >= 0 && x < lines.length && y >= 0 && y < lines[x].length;
+
+let mapFinalTrail = {} as { [key: string]: number };
+
+let sumTrailHead = 0;
+const checkTrail = (x: number, y: number) => {
+  const actualHeight = Number(lines[x][y]);
+
+  if (actualHeight === 9 && mapFinalTrail[`${x},${y}`] === undefined) {
+    mapFinalTrail[`${x},${y}`] = 1;
+    sumTrailHead++;
+  }
+
+  // up
+  if (checkOnLimit(x - 1, y)) {
+    if (Number(lines[x - 1][y]) === actualHeight + 1) {
+      checkTrail(x - 1, y);
+    }
+  }
+
+  // right
+  if (checkOnLimit(x, y + 1)) {
+    if (Number(lines[x][y + 1]) === actualHeight + 1) {
+      checkTrail(x, y + 1);
+    }
+  }
+
+  // down
+  if (checkOnLimit(x + 1, y)) {
+    if (Number(lines[x + 1][y]) === actualHeight + 1) {
+      checkTrail(x + 1, y);
+    }
+  }
+
+  // left
+  if (checkOnLimit(x, y - 1)) {
+    if (Number(lines[x][y - 1]) === actualHeight + 1) {
+      checkTrail(x, y - 1);
+    }
+  }
+};
+
+let sum = 0;
+for (let i = 0; i < lines.length; i++) {
+  for (let j = 0; j < lines[i].length; j++) {
+    if (lines[i][j] === "0") {
+      checkTrail(i, j);
+      sum += sumTrailHead;
+
+      sumTrailHead = 0;
+      mapFinalTrail = {};
+    }
+  }
+}
+
+console.timeEnd("Time to compute");
+
+console.log(sum);
